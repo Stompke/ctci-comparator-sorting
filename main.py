e2d1f4a4e8ff9102bf2347489270d76eb9b91279
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-07-30 16:55:23 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+
+"""
+UPER:
+U:
+    > I DONT UNDERSTAND YET
+    
+"""
+
+
+from functools import cmp_to_key
+class Player:
+    def __init__(self, name, score):
+        
+    def __repr__(self):
+        
+    def comparator(a, b):
